6d003de61e0ce37bb8fa715d69b98dbf958ab1ae
billy lindgren <billylindgren@hotmail.com>
2017-12-12 14:46:28 +0100

    updated rosbag script to new topics

--- unicorn/scripts/record_odom.sh
+++ unicorn/scripts/record_odom.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rosbag record /odom /zed/odom /tf /map /amcl_pose
+rosbag record /odometry/filtered /zed/odom /tf /map /amcl_pose /zed/point_cloud/cloud_registered \
+/ultrasonic_bl /ultrasonic_bml /ultrasonic_brm /ultrasonic_fl /ultrasonic_fml /ultrasonic_fmr /ultrasonic_fr /scan_filtered \
+/cloud_filtered
+
